f75f21f43e81414bdb77a78e1e07a9a8ef2ba787
Matthew Caruana Galizia <mattcg@gmail.com>
2013-02-01 18:06:01 +0000

    Add commandline help

--- lib/CommandLineRunner.php
+++ lib/CommandLineRunner.php
@@ -20,12 +20,13 @@ class CommandLineRunner {
 	const OPT_PRAGMA = 'p';
 	const OPT_DEBUG  = 'd';
 	const OPT_GLOBAL = 'g';
+	const OPT_HELP   = 'h';
 
 	private $debugmode = false;
 	private $debugfunc = null;
 
 	public function getOptions() {
-		return self::OPT_MODULE.':'.self::OPT_GLOBAL.':'.self::OPT_PRAGMA.'::'.self::OPT_DEBUG;
+		return self::OPT_MODULE.':'.self::OPT_GLOBAL.':'.self::OPT_PRAGMA.'::'.self::OPT_DEBUG.self::OPT_HELP;
 	}
 
 	public function getLongOptions() {
@@ -42,9 +43,42 @@ class CommandLineRunner {
 		}
 	}
 
+	private function outputHelp() {
+		echo PHP_EOL, ' ', str_repeat('#', 44), PHP_EOL;
+		echo ' # cjsDelivery', PHP_EOL, ' # Copyright (c) 2012 Matthew Caruana Galizia', PHP_EOL, ' # @mcaruanagalizia', PHP_EOL, PHP_EOL;
+
+		$out = function($opts, $long = false) {
+			$hyphens = $long ? '--' : '-';
+			$indent = str_repeat(' ', 5);
+			foreach ($opts as $opt => &$help) {
+				echo "\033[1m", $indent, $hyphens, $opt, "\033[0m", PHP_EOL, $indent, chunk_split($help, 76, PHP_EOL . $indent), PHP_EOL;
+			}
+		};
+
+		$out(array(
+			self::OPT_MODULE => 'Specify a module by path.',
+			self::OPT_GLOBAL => 'Specify a JavaScript file with contents to be included "globally" so that its symbols are available within the scope of all other other modules. The require function is available within the "global" JavaScript scope.',
+			self::OPT_PRAGMA => 'Turn on a pragma by name.',
+			self::OPT_DEBUG  => 'Show debug messages while processing commands.',
+			self::OPT_HELP   => 'Display this message.'
+		));
+
+		$out(array(
+			self::LONGOPT_MAIN   => 'Specify the main "bootstrap" module that will be automatically required at the end of the output. A module specified using this option will be added automatically so it doesn\'t need to be specified using -' . self::OPT_MODULE . '.',
+			self::LONGOPT_INCLD  => 'Specify the include path as a colon-separated list.',
+			self::LONGOPT_PFMT   => 'Specify the pragma format. Defaults to ' . PragmaManager::DEFAULT_PFMT . '.',
+			self::LONGOPT_MINIFY => 'Use tiny identifiers in output.'
+		), true);
+	}
+
 	public function run(array $options, \Closure $debugfunc) {
+		if (isset($options[self::OPT_HELP])) {
+			$this->outputHelp();
+			return;
+		}
+
 		if (empty($options[self::OPT_MODULE]) and empty($options[self::LONGOPT_MAIN])) {
-			throw new Exception('No module specified', Exception::NOTHING_TO_BUILD);
+			throw new Exception('No module specified. Use -' . self::OPT_HELP . ' for help.', Exception::NOTHING_TO_BUILD);
 		}
 
 		if (isset($options[self::OPT_DEBUG])) {

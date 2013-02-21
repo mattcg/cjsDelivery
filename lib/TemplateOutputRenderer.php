15abb4e3fd2e5baefe71ea6111e4eb23c4a3f6b5
Matthew Caruana Galizia <mattcg@gmail.com>
2013-02-21 17:01:33 -0600

    Render output using concatenation in a heredoc instead of by loading and processing templates

--- lib/TemplateOutputRenderer.php
+++ lib/TemplateOutputRenderer.php
@@ -11,43 +11,18 @@ require_once 'OutputRenderer.php';
 
 class TemplateOutputRenderer implements OutputRenderer {
 
-	const DIR_TEMPLATES = '/templates';
-	const DIR_LIB = __DIR__;
-
-	const TEMPLATE_FULL = 'full';
-	const TEMPLATE_MODULE = 'module';
-	const TEMPLATE_MAIN = 'main';
-
-	const EXT_MS = '.ms';
-
-	private $templates = array();
-
-
-	/**
-	 * Simple template renderer used internally
-	 *
-	 * @param string $templatename
-	 * @param array $keys
-	 * @param array $values
-	 * @return string
-	 */
-	private function renderTemplate($name, &$keys, &$values) {
-		if (!isset($this->templates[$name])) {
-			$filepath = self::DIR_LIB . self::DIR_TEMPLATES . '/' . $name . self::EXT_MS;
-			$this->templates[$name] = file_get_contents($filepath, false);
-		}
-
-		return str_replace($keys, $values, $this->templates[$name]);
-	}
-
 
 	/**
 	 * @see outputRender::renderModule
 	 */
 	public function renderModule(&$module) {
-		$keys = array('{{identifier}}', '{{code}}');
-		$values = array($module->getUniqueIdentifier(), $module->getCode());
-		return $this->renderTemplate(self::TEMPLATE_MODULE, $keys, $values);
+		$identifier = $module->getUniqueIdentifier();
+		$code = $module->getCode();
+		return <<<MODULE
+			modules['$identifier'] = function(require, exports, module, modules) {
+				$code
+			};
+MODULE;
 	}
 
 
@@ -55,13 +30,28 @@ class TemplateOutputRenderer implements OutputRenderer {
 	 * @see OutputRenderer::renderOutput
 	 */
 	public function renderOutput(&$output, $main = '', &$globals = '') {
-		$keys = '{{main}}';
 		if ($main) {
-			$main = $this->renderTemplate(self::TEMPLATE_MAIN, $keys, $main);
+			$main = "require('$main');";
 		}
 
-		$keys = array('{{output}}', '{{main}}', '{{globals}}');
-		$values = array($output, $main, $globals);
-		return $output = $this->renderTemplate(self::TEMPLATE_FULL, $keys, $values);
+		return <<<OUTPUT
+			(function(modules) {
+				var require = function(identifier) {
+					var module, exports, closure;
+					if (!modules[identifier].hasOwnProperty('exports')) {
+						exports = {};
+						module = {id: identifier, exports: exports};
+						closure = modules[identifier];
+						modules[identifier] = module;
+						closure.call(module, require, exports, module);
+					}
+					return modules[identifier].exports;
+				};
+
+				$output
+				$globals
+				$main
+			}({}));
+OUTPUT;
 	}
 }

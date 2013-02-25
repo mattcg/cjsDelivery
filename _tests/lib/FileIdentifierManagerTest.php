dcf617e4f424e18a17dc82ee3d430b6ef102b3e3
Matthew Caruana Galizia <mattcg@gmail.com>
2013-02-25 14:21:55 -0600

    More tests for findFileInDir

--- _tests/lib/FileIdentifierManagerTest.php
+++ _tests/lib/FileIdentifierManagerTest.php
@@ -49,6 +49,56 @@ class FileIdentifierManagerTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
 	}
 
+	public function testIndexJsFileIsFound() {
+		$path = CJSD_TESTMODS_DIR . '/apple';
+
+		$real = realpath($path . '/index.js');
+		$this->assertTrue($real !== false);
+
+		$identifiermanager = $this->getManager();
+		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
+	}
+
+	public function testFileWithSameNameAsContainingDirectoryIsFound() {
+		$path = CJSD_TESTMODS_DIR . '/banana';
+
+		$real = realpath($path . '/banana.js');
+		$this->assertTrue($real !== false);
+
+		$identifiermanager = $this->getManager();
+		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
+	}
+
+	public function testOnlyFileInDirectoryIsFound() {
+		$path = CJSD_TESTMODS_DIR . '/strawberry';
+
+		$real = realpath($path . '/main.js');
+		$this->assertTrue($real !== false);
+
+		$identifiermanager = $this->getManager();
+		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
+	}
+
+	public function testFileSpecifiedInPackageJsonIsFound() {
+		$path = CJSD_TESTMODS_DIR . '/grapefruit';
+
+		$real = realpath($path . '/lib/grapefruit.js');
+		$this->assertTrue($real !== false);
+
+		$identifiermanager = $this->getManager();
+		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
+	}
+
+	public function testIndexJsFileIsChosenOverFileWithSameNameAsDir() {
+		$path = CJSD_TESTMODS_DIR . '/quince';
+
+		$real = realpath($path . '/index.js');
+		$this->assertTrue($real !== false);
+
+		$identifiermanager = $this->getManager();
+		$this->assertEquals($real, $identifiermanager->getTopLevelIdentifier($path));
+	}
+
 
 	/**
 	 * @expectedException PHPUnit_Framework_Error_Notice
